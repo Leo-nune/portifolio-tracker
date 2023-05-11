c1f7dc2452d39c7f1e1ab0d810cd18c8537abf20
Leo-nune <leo7777@terra.com.br>
2023-05-11 20:27:51 -0300

    abc

---
+++ colecionar.js
@@ -0,0 +1,23 @@
+document.querySelector(".remover-vildred").addEventListener("click", () => 
+    document.querySelector(".Vildred").classList.add("hide")
+)
+
+document.querySelector(".remover-diene").addEventListener("click", () => 
+    document.querySelector(".Diene").classList.add("hide")
+)
+
+document.querySelector(".remover-ras").addEventListener("click", () => 
+    document.querySelector(".Ras").classList.add("hide")
+)
+
+document.querySelector(".add-straze").addEventListener("click", () => 
+    document.querySelector(".Straze").classList.add("hide")
+)
+
+document.querySelector(".add-vildred").addEventListener("click", () => 
+    document.querySelector(".Vildred2").classList.add("hide")
+)
+
+document.querySelector(".add-diene").addEventListener("click", () => 
+    document.querySelector(".Diene2").classList.add("hide")
+)
