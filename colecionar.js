bf772c697f75071a4419916d5e6dc76a844c2777
Leo-nune <leo7777@terra.com.br>
2023-05-11 22:14:13 -0300

    abcd

--- colecionar.js
+++ colecionar.js
@@ -1,23 +1,40 @@
-document.querySelector(".remover-vildred").addEventListener("click", () => 
+document.querySelector(".remover-vildred").addEventListener("click", () => {
     document.querySelector(".Vildred").classList.add("hide")
-)
+    document.querySelector(".Vildred2").classList.remove("hide")
+})
 
-document.querySelector(".remover-diene").addEventListener("click", () => 
+document.querySelector(".remover-diene").addEventListener("click", () => {
     document.querySelector(".Diene").classList.add("hide")
-)
+    document.querySelector(".Diene2").classList.remove("hide")
+})
 
-document.querySelector(".remover-ras").addEventListener("click", () => 
+document.querySelector(".remover-ras").addEventListener("click", () => {
     document.querySelector(".Ras").classList.add("hide")
-)
+    document.querySelector(".Ras2").classList.remove("hide")
+})
 
-document.querySelector(".add-straze").addEventListener("click", () => 
+document.querySelector(".remover-straze").addEventListener("click", () => {
     document.querySelector(".Straze").classList.add("hide")
-)
+    document.querySelector(".Straze2").classList.remove("hide")
+})
 
-document.querySelector(".add-vildred").addEventListener("click", () => 
+
+document.querySelector(".add-straze").addEventListener("click", () => { 
+    document.querySelector(".Straze2").classList.add("hide")
+    document.querySelector(".Straze").classList.remove("hide")
+})
+
+document.querySelector(".add-vildred").addEventListener("click", () => {
     document.querySelector(".Vildred2").classList.add("hide")
-)
+    document.querySelector(".Vildred").classList.remove("hide")
+})
 
-document.querySelector(".add-diene").addEventListener("click", () => 
+document.querySelector(".add-diene").addEventListener("click", () => {
     document.querySelector(".Diene2").classList.add("hide")
-)
+    document.querySelector(".Diene").classList.remove("hide")
+})
+
+document.querySelector(".add-ras").addEventListener("click", () => {
+    document.querySelector(".Ras2").classList.add("hide")
+    document.querySelector(".Ras").classList.remove("hide")
+})
